ab8716e479ed6c2902ca5c81a977bf5e87d22ab6
liwei <liwei@liweis-MacBook-Pro.local>
2016-06-07 18:47:18 +0800

    until 10.1.3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      #log_in @user
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end

dbb0d432cc3132ce52af3636fa1d239cec83b556
liwei <liwei@liweis-MacBook-Pro.local>
2016-06-09 15:40:27 +0800

    Add user following

---
+++ db/migrate/20160609005937_create_relationships.rb
@@ -0,0 +1,14 @@
+class CreateRelationships < ActiveRecord::Migration
+  def change
+    create_table :relationships do |t|
+      t.integer :follower_id
+      t.integer :followed_id
+
+      t.timestamps null: false
+    end
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    add_index :relationships, [:follower_id, :followed_id], unique: true
+    # enforce uniqueness so a user cant follow another user more than once
+  end
+end
